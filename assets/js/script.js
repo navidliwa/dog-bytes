cea961ca7dd3c797aa133b24d83950cbc824808f
Riley Allred <riley_allred@hotmail.com>
2023-01-25 21:35:50 -0700

    added rough map

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ $(document).ready(function(){
   $('.modal').modal();
   $('.dropdown-trigger').dropdown();
 });
+
+
+
+function initMap(){
+  // The location of Uluru
+const uluru = { lat: 50, lng: 180 };
+// The map, centered at Uluru
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 4,
+  center: uluru,
+});
+}
+initMap()
