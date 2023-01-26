cce42f3e5974356a5be38f82456d29899a755d92
Jonochi <tana.jayne@gmail.com>
2023-01-26 15:48:49 -0700

    added upload image functionality, test for object to be displayed on tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,47 @@
 var products = [
   {
-      brandName: "Purina One (kibble)",
-      threeToTwelve: 0.5,
-      thirteenToTwenty: 1,
-      twentyOneToThirtyFive: 1.5,
-      thirtySixToFifty: 2,
-      fiftyOneToSeventyFive: 3,
-      seventySixToOneHundred: 4,
-  },{
-      brandName: "Purina One (canned)",
-      threeToTwelve: 0.5,
-      thirteenToTwenty: 1,
-      twentyOneToThirtyFive: 1.5,
-      thirtySixToFifty: 2,
-      fiftyOneToSeventyFive: 3,
-      seventySixToOneHundred: 4,  
+    brandName: "Purina One (kibble)",
+    threeToTwelve: 0.5,
+    thirteenToTwenty: 1,
+    twentyOneToThirtyFive: 1.5,
+    thirtySixToFifty: 2,
+    fiftyOneToSeventyFive: 3,
+    seventySixToOneHundred: 4,
+  }, {
+    brandName: "Purina One (canned)",
+    threeToTwelve: 0.5,
+    thirteenToTwenty: 1,
+    twentyOneToThirtyFive: 1.5,
+    thirtySixToFifty: 2,
+    fiftyOneToSeventyFive: 3,
+    seventySixToOneHundred: 4,
   }
 ]
 
+this.photoData = "";
 
-
-$(document).ready(function(){
+$(document).ready(function () {
   $('.tabs').tabs();
   $('.modal').modal();
   $('.dropdown-trigger').dropdown();
   $('select').formSelect();
+  reloadDogs();
 });
 
 
-$('.save-dog').on('click', function() {
+
+document.querySelector('#photo').addEventListener('change', function(){
+  var reader = new FileReader();
+
+  reader.addEventListener('load', () => {
+    localStorage.setItem('photo', reader.result);
+    photoData = reader.result;
+  })
+
+  reader.readAsDataURL(this.files[0]);
+})
+
+$('.save-dog').on('click', function () {
   var doggyDeets = {
     name: $('#dog-name').val(),
     weight: $('#dog-weight').val(),
@@ -37,17 +50,45 @@ $('.save-dog').on('click', function() {
     food1: $('#food1').val(),
     food1percent: $('#food-1-percent').val(),
     food2: $('#food2').val(),
-    food2percent: $('#food-2-percent').val()
+    food2percent: $('#food-2-percent').val(),
+    photo: photoData,
   }
-  console.log (doggyDeets, typeof(doggyDeets))
- var dogObj = JSON.parse(localStorage.getItem("dogs"))
- if(dogObj == null) dogObj = []
- dogObj.push(doggyDeets)
- localStorage.setItem("dogs", JSON.stringify(dogObj))
+  console.log(doggyDeets, typeof (doggyDeets))
+  var dogObj = JSON.parse(localStorage.getItem("dogs"))
+  if (dogObj == null) dogObj = []
+  dogObj.push(doggyDeets)
+  localStorage.setItem("dogs", JSON.stringify(dogObj))
 
- //todo: append image to localstorage
-
+  reloadDogs();
 })
+
+function reloadDogs() {  
+  var dogs = JSON.parse(localStorage.getItem("dogs"))
+  if(dogs === null) {
+    return;
+  }
+  for (var i = 0; i < dogs.length; i++) {
+
+    // var tabs = document.querySelector("#tabs");
+    // tabs.appendChild(`<li class="tab col s4"><a href="#test${i+1}">'Test ${i+1}'</a></li>
+    // `)
+
+    var currentDog = dogs[i]
+    var element = document.querySelector(`#test${i+1}`);
+    element.innerHTML = JSON.stringify(currentDog);
+
+  }
+} 
+
+
+// Test image preview
+// document.addEventListener("DOMContentLoaded", () => {
+//   var imageDataUrl = localStorage.getItem("photo");
+
+//   if (imageDataUrl) {
+//     document.querySelector("#imgPreview").setAttribute("src", imageDataUrl);
+//   }
+// });
 
 
 // clearing local storage when you remove a dog's profile
